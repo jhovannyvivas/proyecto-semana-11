bdb6cf5e78674a7206894f8df2a711371a8e420e
jhovannyvivas <EDWIN.VIVAS@ALTERU.CO>
2021-08-03 07:21:18 -0500

    formik, yup y semantic funcionando

--- src/App.js
+++ src/App.js
@@ -6,39 +6,58 @@ import {
   Route
 } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Container, Form, Button } from "semantic-ui-react";
 import './App.css';
 import { Footer } from "./components/footer/footer";
 import Header from "./components/header/header";
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
 function App() {
+  //  const llave = '/';
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required(),
+    }),
+    onSubmit: (formData) => {
+      // llave = '/header';
+      console.log(formData);
+    }
+  });
   return (
-    <Router>
-      <Link to="/titulo"><h1>Dirección</h1></Link>
-        <Switch>
-          <Route path="/titulo">
-            <Footer />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Header />
-          </Route>
-        </Switch>
 
-    </Router>
+         <Container>
+          <Form onSubmit={formik.handleSubmit}>
+            <Form.Input type='text' placeholder='nombre' name='name' onChange={formik.handleChange} error={formik.errors.name}/>
+            <Button type='submit'>Registro</Button>
+          </Form>
+        
 
+
+    </Container>  
+    
   );
 }
-function Home() {
-  return <h2>Home</h2>;
-}
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 export default App;
+    // <div>
+
+// {/* <Router>
+
+//       <Link to="/titulo"><h1>Dirección</h1></Link>
+//         <Switch>
+//           <Route path="/titulo">
+//             <Footer />
+//           </Route>
+//           <Route path="/">
+//             <Header />
+//           </Route>
+//         </Switch>
+
+//     </Router>
+
+
+//     </div> */}
